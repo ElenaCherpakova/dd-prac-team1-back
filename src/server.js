0886b992721ee122b0b4570963867f6b3dc8608a
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-07-21 21:14:23 -0400

    connect mongodb and setup server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,29 @@
+require('dotenv').config();
 const { PORT = 8000 } = process.env;
-const app = require("./app");
+const app = require('./app');
+const connectDB = require('./db/connect');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+/* database connection */
+const url = process.env.MONGO_URL;
+const store = new MongoDBStore({
+  uri: url,
+  collection: 'sessions',
+});
+store.on('error', () => {
+  console.log(error);
+});
+
+const listener = () => console.log(`Listening on Port ${PORT}...`);
+
+const start = async () => {
+  try {
+    await connectDB(url);
+    console.log('DB connection established');
+    app.listen(PORT, listener);
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
