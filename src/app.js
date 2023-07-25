c2e980a70b7629f9b0b0e5882fa6f30b189ba3f5
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-07-24 22:33:44 -0400

    setup AI for fetching data based on user input, create controller for that and routes

--- src/app.js
+++ src/app.js
@@ -9,91 +9,12 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const session_params = require('./sessionConfig');
 
-// Initialize OpenAI API
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-app.post('/api/v1/query', async (req, res) => {
-  const query = 'pomegranate';
-  const assistant = `You are a helpful assistant that generates cooking recipes based on user input. It can be just one word and can be several ones.
-  Return response in the following parsable JSON format:
-
-  {
-    "title": "Recipe title",
-    "ingredients": [{
-      "name": "ingredient 1",
-      "quantity": "quantity 1",
-    },
-    {
-      "name": "ingredient 2",
-      "quantity": "quantity 2",
- 
-    },
-    {
-      "name": "ingredient 3",
-      "quantity": "quantity 3",
-      
-    }
-  ],
-    "instructions": [
-      "instruction 1",
-      "instruction 2",
-      "instruction 3"
-    ]
-    servingFor: "quantity" 
-    prepTimeInSeconds: "min"
-    cookTimeInSeconds: "min"
-    totalTimeInSeconds: "min"
-    nutritionInformation: "nutrition information"
-    images: [
-      "image 1",
-    ]
-  }
-`;
-  console.log(query);
-  try {
-    const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'system',
-          content: assistant,
-        },
-        {
-          role: 'user',
-          content: `User asks: Give me a recipe with ${query}.`,
-        },
-      ],
-
-      temperature: 1,
-      max_tokens: 356,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-
-    const completionMessage = response.data.choices[0].message.content;
-    console.log(completionMessage);
-    const responseObject = JSON.parse(completionMessage);
-    console.log(responseObject);
-    return res.json({ response: responseObject });
-  } catch (error) {
-    console.error(error);
-  }
-});
-
 /*security packages*/
 const cors = require('cors');
 const favicon = require('express-favicon');
 const logger = require('morgan');
 const rateLimiter = require('express-rate-limit');
 const helmet = require('helmet');
-
-/*routes*/
-const mainRouter = require('./routes/mainRouter.js');
-const authRouter = require('./routes/auth_routes.js');
 
 /* middleware */
 app.use(cookieParser());
@@ -108,6 +29,13 @@ app.use(
     max: 100, // limit each IP to 100 requests per windowMs
   })
 );
+
+
+
+/*routes*/
+const mainRouter = require('./routes/mainRouter.js');
+const authRouter = require('./routes/auth_routes.js');
+const aiRecipeRouter = require('./routes/apiRecipe_routes')
 
 app.use(express.static('public'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -125,5 +53,6 @@ app.get('/api/v1/test', (req, res) => {
 /* routes */
 app.use('/api/v1', mainRouter);
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/ai-recipe', aiRecipeRouter);
 
 module.exports = app;
