c00fe6e39627e51ad679bab80d75ec4efdf09d23
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-08-02 19:18:25 -0400

    add asyncWrapper in order to avoid redundancy of try catch

---
+++ src/controllers/addApiRecipe_contoller.js
@@ -0,0 +1,7 @@
+const Recipe = require('../models/Recipe');
+const asyncWrapper = require('../middleware/async');
+
+const addApiRecipe = asyncWrapper(async (req, res) => {
+    
+
+});
--- src/routes/apiRecipe_routes.js
+++ src/routes/apiRecipe_routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const { fetchApiRecipe } = require('../controllers/aiRecipe_controller');
-
+const {addApiRecipe} = require('../controllers/')
 router.post('/', fetchApiRecipe);
+router.post('/add', addApiRecipe);
 
 module.exports = router;
